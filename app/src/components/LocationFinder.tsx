95b88626ae4006c6470d1a57a03ae4dec4a194e8
Will Smith <willsmithte@gmail.com>
2022-08-13 15:04:04 +0200

    improve location finder

--- app/src/components/LocationFinder.tsx
+++ app/src/components/LocationFinder.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import { subHeader } from "../services/styles"
 import { FullScreenLoader } from "./Loading"
 import { Text, View, StyleSheet, Dimensions, KeyboardAvoidingView, Platform } from 'react-native'
@@ -35,7 +35,8 @@ function useLoader() {
 }
 export const LocationFinder = () => {
     const [places, setPlaces] = useState<Place[] | undefined>()
-    const [place, setPlace] = useState<Place | undefined>()
+    const [selectedPlace, setSelectedPlace] = useState<Place | undefined>()
+    const [initialPlace, setInitialPlace] = useState<Place | undefined>()
     const [yesNo1, setYesNo1] = useState<'yes' | 'no' | undefined>()
     const [howAboutHere, setHowAboutHere] = useState<string | undefined>()
     const loader = useLoader()
@@ -55,19 +56,27 @@ export const LocationFinder = () => {
     }, [userLocation])
 
     useEffect(() => {
-        if (places !== undefined && places.length > 0) setPlace(places[0])
+        if (places !== undefined && places.length > 0) {
+            setSelectedPlace(places[0])
+            setInitialPlace(places[0])
+        }
     }, [places])
 
     useEffect(() => {
-        howAboutHere !== undefined && howAboutHere !== 'no' && setPlace(places?.find(({ id }) => id === howAboutHere))
+        setSelectedPlace(places?.find(({ id }) => id === howAboutHere))
     }, [howAboutHere])
+
+    useMemo(() => {
+        if (yesNo1 === 'no') setSelectedPlace(undefined)
+        else setSelectedPlace(initialPlace)
+    }, [yesNo1])
 
     const { control, handleSubmit, formState: { errors }, setValue } = useForm<{ resort: string }>();
 
     const setResortFromBackend = (id: string) => {
         tryCatchAsync(
             () => loader.findResort(id),
-            setPlace,
+            setSelectedPlace,
             setError,
         )
     }
@@ -86,14 +95,15 @@ export const LocationFinder = () => {
                 if (error) {
                     return <Text>oops</Text>
                 } else if (userLocation && (places !== undefined)) {
-                    const region = place ? googlePlaceToRegion(place.googlePlace, mapWidth, mapHeight) : userLocationToRegion(userLocation)
+                    const region = selectedPlace ? googlePlaceToRegion(selectedPlace.googlePlace, mapWidth, mapHeight) : userLocationToRegion(userLocation)
                     return <>
-                        <MapView provider={Device.isDevice ? PROVIDER_GOOGLE : undefined} style={styles.map} region={region} showsUserLocation={true}>
-                            {place && <Marker coordinate={locationToLatLng(place.googlePlace.geometry.location)} />}
-                        </MapView>
-                        <KeyboardAvoidingView style={styles.form} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-                            {place ? <>
-                                <Text style={subHeader}>Are you skiing at {place.name} today?</Text>
+                        {selectedPlace && <MapView provider={Device.isDevice ? PROVIDER_GOOGLE : undefined} style={styles.map} region={region} showsUserLocation={true}>
+                            {selectedPlace && <Marker coordinate={locationToLatLng(selectedPlace.googlePlace.geometry.location)} />}
+                        </MapView>}
+                        <KeyboardAvoidingView style={[styles.form, !selectedPlace && { marginTop: 60 }]}
+                            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+                            {initialPlace ? <>
+                                <Text style={subHeader}>Are you skiing at {initialPlace.name} today?</Text>
                                 <SingleSelector selected={yesNo1} set={setYesNo1} options={yesNoOptions} />
                             </> :
                                 <>
@@ -105,7 +115,7 @@ export const LocationFinder = () => {
                                     options={howAboutHere === undefined ? Array.from(resortSelectionOptions.values()) :
                                         [resortSelectionOptions.get(howAboutHere)!!]} />
                             </>}
-                            {(place === undefined || howAboutHere === 'no' || (yesNo1 === 'no' && places.length === 1)) && <SkiResortSelector />
+                            {(howAboutHere === 'no' || (yesNo1 === 'no' && places.length === 1)) && <SkiResortSelector />
                             }
                             <View style={{ flex: 1 }} />
                         </KeyboardAvoidingView>
@@ -130,5 +140,9 @@ const styles = StyleSheet.create({
         width: mapWidth,
         height: mapHeight,
     },
-    form: { flex: 1, paddingHorizontal: 20, justifyContent: 'flex-end' },
+    form: {
+        flex: 1,
+        paddingHorizontal: 20,
+        justifyContent: 'flex-end',
+    },
 })
