2bb9746364a95ec7eefccc4e51defe20f88a7116
Will Smith <willsmithte@gmail.com>
2022-08-24 16:46:25 +0200

    add email sender

--- backend/src/service/EmailService.ts
+++ backend/src/service/EmailService.ts
@@ -1,5 +1,51 @@
+import hbs, { NodemailerExpressHandlebarsOptions } from 'nodemailer-express-handlebars'
+import nodemailer from 'nodemailer'
+import path from 'path'
+
+const createTransport = () => nodemailer.createTransport(
+    {
+        service: 'gmail',
+        auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD,
+        }
+    }
+);
+const createHandlebarOptions: () => NodemailerExpressHandlebarsOptions = () => ({
+    viewEngine: {
+        partialsDir: path.resolve('./src/emailViews/'),
+        defaultLayout: false,
+    },
+    viewPath: path.resolve('./src/emailViews/'),
+})
+
 export const emailService = {
     sendWelcome: (email: string) => {
-        console.log('welcome ' + email)
+        const transport = createTransport()
+        const handlebarOptions = createHandlebarOptions()
+        transport.use('compile', hbs(handlebarOptions))
+        const mailOptions = {
+            from: `"Shred Crew" <${process.env.EMAIL_USERNAME}>`,
+            to: email,
+            subject: 'Welcome to the Shred Crew',
+            template: 'email', // the name of the template file i.e email.handlebars
+            context: {
+                // name: "Adebola", // replace {{name}} with Adebola
+            },
+            attachments: [{
+                filename: 'icon.png',
+                path: `./src/emailViews/icon.png`,
+                cid: 'icon'
+            }],
+        };
+        transport.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error)
+            }
+            console.log('Message sent: ' + info.response);
+        });
+
     },
 }
+
+emailService.sendWelcome('willsmithte@gmail.com')
